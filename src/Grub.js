1681771b52f73cc70152ef4f6bee80efaab87d30
Bryan Newby <brnewby602@gmail.com>
2016-03-07 13:18:42 -0800

    Pass all tests for 6ees6ees6ees objects

--- src/Grub.js
+++ src/Grub.js
@@ -1,4 +1,13 @@
 class Grub {
+  constructor() {
+    this.food = 'jelly';
+    this.age = 0;
+    this.color = 'pink';
+  }
+
+  eat() {
+    return 'Mmmmmmmmm ' + this.food;
+  }
 };
 
 export default Grub;
